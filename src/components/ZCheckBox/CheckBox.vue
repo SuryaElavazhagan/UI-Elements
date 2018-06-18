c595abf37ab25a9280a631a69019cd61b30a22b1
SuryaElavazhagan <surya.elash98@protonmail.com>
2018-06-18 13:26:52 +0530

    Initiated Checkbox component

---
+++ src/components/ZCheckBox/CheckBox.vue
@@ -0,0 +1,28 @@
+<template>
+    <label class="root">
+        <span role="checkbox" aria-checked="mixed" class="z-checkbox"></span>
+        <input aria-hidden="true" type="checkbox" hidden v-model="checked"/>
+        <span class="label">{{ label }}</span>
+    </label>
+</template>
+
+<script>
+export default {
+    name : 'ZCheckBox',
+    props : {
+        label : String
+    },
+    data(){
+        return{
+            checked : false
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../../style/checkbox";
+
+</style>
+
