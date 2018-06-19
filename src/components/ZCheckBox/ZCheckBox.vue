cee9263d68ffcfdd3dfaa0c7de5791322d2f7253
SuryaElavazhagan <surya.elash98@protonmail.com>
2018-06-19 19:11:08 +0530

    Experimenting file

---
+++ src/components/ZCheckBox/ZCheckBox.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="checkbox">
+        <label :class="{'disabled' :  disabled}">
+            <input type="checkbox" :checked="checked" :value="value">
+            <span class="custom-checkbox">
+                <i class="icon-check"></i>
+            </span>
+            {{label}}
+        </label>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'ZCheckBoxComponent',
+    props : {
+        label : {
+            type : String,
+            required : true
+        },
+        value : {
+            type : Object | Array,
+            required : true
+        },
+        checked : {
+            type : Boolean
+        },
+        disabled : {
+            type : Boolean,
+            default : false
+        }
+    }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
